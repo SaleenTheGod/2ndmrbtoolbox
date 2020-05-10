fa387d22bfce95a9593b4903d2483a2f832f16e5
James Ambrose <galaxy2198@gmail.com>
2020-05-10 14:30:07 -0500

    made service files and made mission automation better

--- moveArmaMissions.sh
+++ moveArmaMissions.sh
@@ -1,15 +1,10 @@
 #! /bin/bash
 
-if [ -z "$1" ]
-then
-    sudo mv *.pbo /home/arma3server/serverfiles/mpmissions/
-    sudo chown -R arma3server:users /home/arma3server/serverfiles/mpmissions/
-else
-    if [ $1 = "restart" ]
-    then
-        sudo mv *.pbo /home/arma3server/serverfiles/mpmissions/
-        sudo chown -R arma3server:users /home/arma3server/serverfiles/mpmissions/   
-        sudo runuser -l arma3server -c '/home/arma3server/arma3server restart'
-    fi
-fi
-
+count=`ls -1 /home/centos/*.pbo 2>/dev/null | wc -l`
+if [ $count != 0 ]
+then 
+    echo "New Arma Mission detected, moving to Arma server mpmissions folder and restarting."
+    sudo mv /home/centos/*.pbo /home/arma3server/serverfiles/mpmissions/
+    sudo chown -R arma3server:users /home/arma3server/serverfiles/mpmissions/   
+    sudo runuser -l arma3server -c '/home/arma3server/arma3server restart'
+fi 
